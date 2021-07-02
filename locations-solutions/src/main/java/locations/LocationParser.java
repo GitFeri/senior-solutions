385228eb0936326b9f91d38ab427abc2077ca246
GitFeri <68915209+GitFeri@users.noreply.github.com>
2021-07-02 21:04:38 +0200

    Location some test

--- locations-solutions/src/main/java/locations/LocationParser.java
+++ locations-solutions/src/main/java/locations/LocationParser.java
@@ -3,8 +3,9 @@ package locations;
 public class LocationParser {
     public Location Parse(String text){
     String[] attributes = text.split(",");
-    return new Location(attributes[0],Double.parseDouble(attributes[1]),Double.parseDouble(attributes[2]));
+    return new Location(attributes[0].trim(),
+            Double.parseDouble(attributes[1].trim()),
+            Double.parseDouble(attributes[2].trim()));
     }
-
 }
 
