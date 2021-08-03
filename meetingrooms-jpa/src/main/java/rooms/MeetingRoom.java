c8a6773ccab7f900addb183bd45eb28ce47670d7
GitFeri <68915209+GitFeri@users.noreply.github.com>
2021-08-03 22:02:56 +0200

    MeetingRoom

---
+++ meetingrooms-jpa/src/main/java/rooms/MeetingRoom.java
@@ -0,0 +1,28 @@
+package rooms;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class MeetingRoom {
+
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private int width;
+    private int length;
+
+    public MeetingRoom(String name, int width, int length) {
+        this.name = name;
+        this.width = width;
+        this.length = length;
+    }
+}
